b3c3d74c9529c8bf13e90756dea9abd984ffa3fe
app1 <saieesh.soma1919@gmail.com>
2024-03-29 12:43:07 +0530

    month

--- src/App.js
+++ src/App.js
@@ -1,52 +1,36 @@
 import React,{useState} from "react";
-// import images to use it 
 import summer from "./images/summer.jpg"
 import winter from "./images/winter.jpg"
 import rainy from "./images/rainy.jpg"
 
 const App=()=>{
 
-     const [latitude,setLatitude]=useState(0.00) // update the varable
+     const [latitude,setLatitude]=useState(0.00) 
      const [longitude,setLongitude]=useState(0.00)
      const [hemisphere,sethemisphere]=useState("NA")
-    //  const [month,setMonth]=useState(()=>{return new Date().getMonth()+1})
-    const [month,setMonth]=useState(8)
-    
-     // hemisphere could only be updated by sethemisphere function
-
-   // Normal variables we dont use it to update in react
-
+     const [month,setMonth]=useState(()=>{return new Date().getMonth()+1})
 
     function fetchLocation(){
-        if(navigator.geolocation){// checking if this feature exists in browser
-            navigator.geolocation.getCurrentPosition(
-                (position)=>{
-                    //  console.log(position.coords.latitude)
-                    //  console.log(position.coords.longitude)
-
-                    setLatitude(position.coords.latitude)
-                    setLongitude(position.coords.longitude)
+        if(navigator.geolocation)
+        {
+            navigator.geolocation.getCurrentPosition((position)=>
+            {
+              setLatitude(position.coords.latitude)
+              setLongitude(position.coords.longitude)
                      
-                    if(position.coords.latitude>0){
-                        // if lat>o it will be Nothen
-                        // adding nort
-                        sethemisphere("Northern Hemisphere")
-                    }
-                    else if (position.coords.latitude<0){
-                        sethemisphere("Southern Hemisphere")
-                    }
-
-                    else{
-                        sethemisphere("Equator")
-                    }
-
-                    
+                if(position.coords.latitude>0)
+                {
+                  sethemisphere("Northern Hemisphere")
                 }
-            )
+                else if (position.coords.latitude<0)
+                {
+                  sethemisphere("Southern Hemisphere")
+                }
+                else{sethemisphere("Equator")}       
+            })
 
         }
     }
-
 
 return (
     <div>
@@ -56,19 +40,16 @@ return (
         <h1>Hemisphere:{hemisphere}</h1>
         <h1>Month:{month}</h1>
 
-        {
+          {
             hemisphere && ((hemisphere=="Northern Hemisphere" && (month>=3 && month<=6))||
-                (hemisphere=="Southern Hemisphere" && (month>=3 && month<=6))
-            )
-
-            && 
-            (
+              (hemisphere=="Southern Hemisphere" && (month>=3 && month<=6)))
+            &&(
                 <div>
                     <h1>Welcome to Summer Season</h1>
                     <img src={summer} alt="summer" width={800} height={500}/>
                  </div>
             )
-        }
+          }
           
           {
             hemisphere && ((hemisphere=="Northern Hemisphere" && (month<=2 || month>=12))||
@@ -77,10 +58,8 @@ return (
                 <div>
                   <h1>Welcome to Winter Season</h1>
                   <img src={winter} alt="winter" width={800} height={500}/>
-
                 </div>
               )
-
           }
           {
             hemisphere && ((hemisphere=="Northern Hemisphere" && (month<=11 && month>=7))||
@@ -89,27 +68,12 @@ return (
                 <div>
                   <h1>Welcome to Rainy Season</h1>
                   <img src={rainy} alt="rainy" width={800} height={500}/>
-
                 </div>
               )
 
           }
-
-
     </div>
 )
-
-
-
-
 }
 
-
 export default App;
-
-
-
-
-// if latitude>0->Northern Hemisphere
-// if latitude<0->southern Hemisphere
-// if latitude=0->Equator
